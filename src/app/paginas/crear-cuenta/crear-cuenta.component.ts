d52a565cc25debab347a237ab5c870c9193cd543
sharol2929 <sharol3222@gmail.com>
2023-08-28 07:44:46 -0500

    siguiente version

--- src/app/paginas/crear-cuenta/crear-cuenta.component.ts
+++ src/app/paginas/crear-cuenta/crear-cuenta.component.ts
@@ -7,11 +7,14 @@ import { FormBuilder, Validators } from '@angular/forms';
   styleUrls: ['./crear-cuenta.component.css']
 })
 export class CrearCuentaComponent {
-  
+
+  constructor(private formBuild:FormBuilder){
+  }
+
   inicioerror:string="";
   crearform=this.formBuild.group({
     usuario:['',[Validators.required]],
-    email:['',[Validators.required, Validators.email]],
+    correo:['',[Validators.required, Validators.email]],
     contraseña:['',[Validators.required, Validators.minLength(8)]],
     contraseña2:['',[Validators.required, Validators.minLength(8)]],
     nombre:['',[Validators.required,]],
@@ -20,12 +23,29 @@ export class CrearCuentaComponent {
     fecha:['', Validators.required]
 
   })
-  constructor(private formBuild:FormBuilder){}
+  get nombreNoValido(){
+    return this.crearform.get('usuario')?.invalid && this.crearform.get('usuario')?.touched;
+  } 
+  get correoNoValido(){
+    return this.crearform.get('correo')?.invalid && this.crearform.get('correo')?.touched;
+  }
+  get contrasenaNoValido(){
+    return this.crearform.get('contraseña')?.invalid && this.crearform.get('contraseña')?.touched;
+  }   
+  get contrasena2NoValido(){
+    return this.crearform.get('contraseña2')?.invalid && this.crearform.get('contraseña2')?.touched;
+  }  
+  get nameNoValido(){
+    return this.crearform.get('nombre')?.invalid && this.crearform.get('nombre')?.touched;
+  }  
+  get apellidoNoValido(){
+    return this.crearform.get('apellido')?.invalid && this.crearform.get('apellido')?.touched;
+  }  
+  get fechaNoValido(){
+    return this.crearform.get('genero')?.invalid && this.crearform.get('genero')?.touched;
+  }
   
-  get email(){
-    return this.crearform.controls.email;
-  } 
-  get password(){
-    return this.crearform.controls.contraseña;
-  } 
+  
+  
+  
 }
